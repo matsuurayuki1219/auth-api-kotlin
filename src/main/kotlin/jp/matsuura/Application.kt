cf7500fd046f6adaf8a0517fd525223af1c236f3
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2023-03-21 14:36:19 +0900

    feat: add features.

--- src/main/kotlin/jp/matsuura/Application.kt
+++ src/main/kotlin/jp/matsuura/Application.kt
@@ -1,16 +1,19 @@
 package jp.matsuura
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
 import io.ktor.server.tomcat.*
 import jp.matsuura.controller.authController
 import jp.matsuura.controller.testController
 import jp.matsuura.controller.userController
-import jp.matsuura.di.loginServiceModule
-import jp.matsuura.di.registerServiceModule
+import jp.matsuura.di.authServiceModule
 import org.koin.ktor.plugin.Koin
-
+import java.text.DateFormat
 
 fun main() {
     embeddedServer(Tomcat, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -24,8 +27,14 @@ fun Application.module() {
         userController()
     }
     install(Koin) {
-        modules(loginServiceModule, registerServiceModule)
+        modules(authServiceModule)
     }
-
-
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
+            dateFormat = DateFormat.getDateInstance()
+        }
+    }
 }
