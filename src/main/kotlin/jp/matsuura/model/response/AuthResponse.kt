618f3e8b61a838ac2b7937deded0a5ac72af6a6e
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2023-03-25 16:17:09 +0900

    fix: unify json filed to snake case.

--- src/main/kotlin/jp/matsuura/model/response/AuthResponse.kt
+++ src/main/kotlin/jp/matsuura/model/response/AuthResponse.kt
@@ -1,7 +1,11 @@
 package jp.matsuura.model.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class AuthResponse(
     val message: String,
+    @JsonProperty("access_token")
     val accessToken: String,
+    @JsonProperty("refresh_token")
     val refreshToken: String,
 )
